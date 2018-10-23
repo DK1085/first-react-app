116b86f18e7fdf2ecd2efc4e5799b53481aff5a3
DK1085 <dan.krishnan01@gmail.com>
2018-10-23 22:44:13 +0100

    react course so far

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: 'Dan', age: 33},
+      {name: 'Emily', age: 28}
+    ]
+  }
+
+switchNameHandler = (newName) => {
+    //console.log('was clicked');
+    // DONT DO THIS this.state.persons[0].name = 'Dhani';
+    this.setState({
+      persons: [
+        {name: newName, age: 33},
+        {name: 'Emily', age: 28}]
+  });
+}
+
+nameChangedHandler = (event) => {
+  this.setState({
+    persons: [
+      {name: event.target.value, age: 33},
+      {name: 'Emily', age: 28}
+    ]
+  })
+}
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+       <h1>Hi, I'm a React App</h1>
+       <p>This is working also</p>
+       <button onClick={() => this.switchNameHandler('Dhani!')}>Switch Name</button>
+       <Person
+       name={this.state.persons[0].name}
+       age={this.state.persons[0].age}
+       click={this.switchNameHandler.bind(this, 'Dan!')}
+       changed={this.nameChangedHandler}/>
+       <Person
+       name={this.state.persons[1].name}
+       age={this.state.persons[1].age}/>
       </div>
     );
   }
